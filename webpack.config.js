32dccc2d29a0fa4ff95e02881f03978853f50491
chenzhen0735 <510586202@qq.com>
2020-04-19 19:19:29 +0800

    更新了

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 module.exports = {
    entry: "./index.js",
+   devServer: {
+      contentBase: './dist',
+      hot: true,
+      compress: true,
+      port: 9000,
+      clientLogLevel: "none",
+      quiet: true
+    },
     output: {
        path: path.resolve(__dirname, 'dist'),
        filename: "build.js"
